6e922c4946234263037063a9e1277e7a60497f05
carrottechplus <blohzzang@gmail.com>
2023-06-29 22:52:47 +0900

    메인, 헤더 자잘한 수정

--- js/main.js
+++ js/main.js
@@ -62,3 +62,28 @@ const totop = document.querySelector('.btn_totop');
 totop.addEventListener('click', () => {
 	window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+// const app = (() => {
+// 	let body;
+// 	let menu;
+// 	let menuItems;
+
+// 	const init = () => {
+// 		body = document.querySelector('body');
+// 		menu = document.querySelector('.menu-icon');
+// 		menuItems = document.querySelectorAll('.nav__list-item');
+
+// 		applyListeners();
+// 	};
+
+// 	const applyListeners = () => {
+// 		menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+// 	};
+
+// 	const toggleClass = (element, stringClass) => {
+// 		if (element.classList.contains(stringClass)) element.classList.remove(stringClass);
+// 		else element.classList.add(stringClass);
+// 	};
+
+// 	init();
+// })();
