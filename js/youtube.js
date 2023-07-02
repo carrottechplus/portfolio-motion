5336a7120c20f289ac8b1843f1bee3ba1f591122
carrottechplus <blohzzang@gmail.com>
2023-07-03 02:22:45 +0900

    youtube js 수정 member페이지 fadein 효과

--- js/youtube.js
+++ js/youtube.js
@@ -1,68 +1,52 @@
 const wrap = document.querySelector('.sub_wrap .youtube_wrap .gallery_list');
-const key = 'AIzaSyC4TpEbx2d9lOtjiVQIg3b6wA6ZKKrDL7c';
-const list = 'PLQZTVbf9_qAn_Nwrz2maZG64AaEBcFZfb';
-const num = 10;
-const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
+
+fetchData();
 
 document.body.addEventListener('click', (e) => {
-	// e.preventDefault();
-	console.log(e.target.className === 'gallery_item');
-	if (e.target.className === 'gallery_item') createPop(e.target.getAttribute('data-id'));
-
+	// console.log(e.target.className === 'gallery_item');
+	if (e.target.className === 'gallery_item__img') createPop(e.target.getAttribute('data-id'));
 	if (e.target.className === 'close') removePop();
 });
 
-// 유튜브 플레이 리스트 가져오기 (fetch)
-// 가져온 리스트 화면에 뿌리기
+async function fetchData() {
+	const key = 'AIzaSyC4TpEbx2d9lOtjiVQIg3b6wA6ZKKrDL7c';
+	const list = 'PLQZTVbf9_qAn_Nwrz2maZG64AaEBcFZfb';
+	const num = 10;
+	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
 
-fetch(url)
-	.then((data) => data.json())
-	.then((json) => {
-		//json으로 변환한 데이타 처리 => innerHtml로 tag를 만든다.
-		// console.log(json);
-		// console.log(json.items);
+	const data = await fetch(url);
+	const json = await data.json();
 
-		createList(json);
-	});
+	createList(json.items);
+}
 
-// async function fetchData() {
-// 	const key = 'AIzaSyC4TpEbx2d9lOtjiVQIg3b6wA6ZKKrDL7c';
-// 	const list = 'PLQZTVbf9_qAn_Nwrz2maZG64AaEBcFZfb';
-// 	const num = 10;
-// 	const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${list}&key=${key}&maxResults=${num}`;
-
-// 	const data = await fetch(url);
-// 	const json = await data.json();
-// 	createList(json);
-// }
-
-function createList(json) {
+function createList(arr) {
 	let tags = '';
-	json.items.forEach((data) => {
-		// console.log(data);
-		const title = data.snippet.title;
-		const desc = data.snippet.description;
-		const name = data.snippet.videoOwnerChannelTitle;
-		const date = data.snippet.publishedAt;
+	arr.forEach((item) => {
+		// console.log(item);
+		const title = item.snippet.title;
+		const desc = item.snippet.description;
+		const name = item.snippet.videoOwnerChannelTitle;
+		const date = item.snippet.publishedAt;
 
-		const channelId = data.snippet.resourceId.videoId;
-		const thumbUrl = data.snippet.thumbnails.standard.url;
+		const channelId = item.snippet.resourceId.videoId;
+		const thumbUrl = item.snippet.thumbnails.standard.url;
 
 		tags += `
         <li>
-          <div class='gallery_item' data-id='${channelId}'>
-						<div class='gallery_item__img'>
+          <div class='gallery_item'>
+						<div class='gallery_item__img' data-id='${channelId}'>
 							<img src='${thumbUrl}' class='gallery_img'>
 						</div>
 						<div class='gallery_item__side gallery_side'>
-            <h2 class='gallery_side__title'>${title.length > 70 ? title.substr(0, 90) + '...' : title}</h2>
-            <p class='gallery_side__desc'>${desc.length > 200 ? desc.substr(0, 200) + '...' : desc}</p>
-            
-            <div class='gallery_side__info gallery_info'>
-              <span class='gallery_info__name'>${name}</span>
-              <span class='gallery_info__date'>${date.split('T')[0].split('-').join('.')}</span>
-            </div>
-          </div>
+							<h2 class='gallery_side__title'>${title.length > 70 ? title.substr(0, 90) + '...' : title}</h2>
+							<p class='gallery_side__desc'>${desc.length > 200 ? desc.substr(0, 200) + '...' : desc}</p>
+							
+							<div class='gallery_side__info gallery_info'>
+								<span class='gallery_info__name'>${name}</span>
+								<span class='gallery_info__date'>${date.split('T')[0].split('-').join('.')}</span>
+							</div>
+						</div>
           </div>
         </li>
       `;
@@ -71,9 +55,7 @@ function createList(json) {
 	wrap.innerHTML = tags;
 }
 
-//동적으로 팝업 생성함수
 function createPop(id) {
-	// console.log(id);
 	const tags = `	
 			<div class='con'>
 				<iframe src='https://www.youtube.com/embed/${id}' ></iframe>
@@ -86,7 +68,6 @@ function createPop(id) {
 
 	document.body.append(pop);
 	setTimeout(() => document.querySelector('.pop').classList.add('on'), 0);
-
 	document.body.style.overflow = 'hidden';
 }
 
